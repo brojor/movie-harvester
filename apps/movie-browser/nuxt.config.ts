bf4a538dc81ed4f352b5a4639b57fc631595cd51
brojor <bronislav.jordan@gmail.com>
2025-05-26 14:43:04 +0200

    feat(deps): integrate UnoCSS into movie-browser
    - add @unocss/nuxt to modules in nuxt.config.ts
    - include @unocss/reset/tailwind.css in global CSS

--- apps/movie-browser/nuxt.config.ts
+++ apps/movie-browser/nuxt.config.ts
@@ -3,8 +3,9 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  css: ['@unocss/reset/tailwind.css'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@vueuse/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@vueuse/nuxt', '@unocss/nuxt'],
   eslint: {
     config: {
       standalone: false,
