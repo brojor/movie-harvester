c819e5c0985605590a7b9eaf444a534a6dbc87f0
brojor <bronislav.jordan@gmail.com>
2025-05-26 14:34:28 +0200

    feat(deps): add @vueuse/core and @vueuse/nuxt to movie-browser
    - include @vueuse/core and @vueuse/nuxt in dependencies
    - modify nuxt.config.ts to include @vueuse/nuxt module

--- apps/movie-browser/nuxt.config.ts
+++ apps/movie-browser/nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@vueuse/nuxt'],
   eslint: {
     config: {
       standalone: false,
