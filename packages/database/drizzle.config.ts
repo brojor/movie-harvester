5db23c06a8aa552141025f7b821b7a6957a1b079
brojor <bronislav.jordan@gmail.com>
2025-06-15 20:18:04 +0200

    fix(database): update import path for env module

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,5 +1,6 @@
-import { env } from '@repo/shared'
 import { defineConfig } from 'drizzle-kit'
+// eslint-disable-next-line antfu/no-import-dist
+import { env } from '../shared/dist/index.js'
 
 export default defineConfig({
   schema: './dist/db/schema/*.js',
