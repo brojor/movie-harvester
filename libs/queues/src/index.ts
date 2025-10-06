0ea941349e5701c9540c17eff4818016bc94a1bd
brojor <bronislav.jordan@gmail.com>
2025-10-06 23:38:20 +0200

    refactor(queues): rename name property to bundleName in DownloadJobData
    - change property name from "name" to "bundleName" for better semantic clarity
    - improve interface specificity to indicate the property represents a bundle name

--- libs/queues/src/index.ts
+++ libs/queues/src/index.ts
@@ -6,7 +6,7 @@ import { ControlBus } from './control-bus.js'
 // Define types for download jobs
 export interface DownloadJobData {
   url: string
-  name: string
+  bundleName: string
 }
 
 export interface DownloadJobResult {
