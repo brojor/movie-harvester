57fa31ca5e20da54e9614ff0417b2b48a6a0f8af
brojor <bronislav.jordan@gmail.com>
2025-10-07 13:46:59 +0200

    refactor(config): simplify ecosystem configuration to single webshare worker
    - remove unused worker applications (csfd, rt, tmdb, warforum-indexer)
    - remove movie-browser and download-manager applications
    - update webshare-worker script path to new monorepo structure
    - remove module type declaration from package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,55 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: 'csfd-worker',
-      script: './csfd-worker/index.js',
-      env: {
-        ENV_FILE: './.env',
-      },
-    },
-    {
-      name: 'rt-worker',
-      script: './rt-worker/index.js',
-      env: {
-        ENV_FILE: './.env',
-      },
-    },
-    {
-      name: 'tmdb-worker',
-      script: './tmdb-worker/index.js',
-      env: {
-        ENV_FILE: './.env',
-      },
-    },
-    {
       name: 'webshare-worker',
-      script: './webshare-worker/index.js',
-      env: {
-        ENV_FILE: './.env',
-      },
-    },
-    {
-      name: 'warforum-indexer',
-      script: './warforum-indexer/index.js',
-      autorestart: false,
-      env: {
-        ENV_FILE: './.env',
-      },
-    },
-    {
-      name: 'movie-browser',
-      script: './movie-browser/server/index.mjs',
-      env: {
-        ENV_FILE: './.env',
-      },
-    },
-    {
-      name: 'download-manager',
-      script: './download-manager/server/index.mjs',
-      env: {
-        ENV_FILE: './.env',
-        PORT: 3001,
-      },
+      script: './apps/workers/webshare/dist/index.js',
     },
   ],
 }
